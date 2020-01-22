0ac3e59a2d06c5089d86f9f181e346801c9d845f
Gabe Wils <gabrielmwilson98@gmail.com>
2020-01-22 10:33:24 -0500

    Updated manage.py

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
+    print("In the main method")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
     try:
         from django.core.management import execute_from_command_line
